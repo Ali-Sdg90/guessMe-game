38611c9b7c1d76790790a7bc72d0b48c7eed000e
Ali Sadeghi <ali909392@gmail.com>
2023-09-20 22:21:48 +0330

    Final Tidy Ups / Add comments

--- main.js
+++ main.js
@@ -4,79 +4,57 @@ const restartBtn = document.querySelector(".restartBtn");
 const keyboardBtns = document.querySelectorAll("keyboard button");
 const wordToFind = document.querySelector(".wordToFind");
 const endGameScreen = document.querySelector(".endGame");
-const names = [
-    "ریحانه",
-    "نیلوفر",
-    "نازنین",
-    "کیمیا",
-    "شهرزاد",
-    "فرناز",
-    "مهرناز",
-    "مژگان",
-    "معصومه",
-    "رومینا",
-    "گلچهره",
-    "دلاویز",
-    "سمانه",
-    "پریناز",
-    "بهنوش",
-    "سپیده",
-    "سعیده",
-    "سمیرا",
-    "گلشیفته",
-    "گلناز",
-    "بارانا",
-    "علیرضا",
-    "رادوین",
-    "ارشیا",
-    "امیرعلی",
-    "سامیار",
-    "عرفان",
-    "ماکان",
-    "سیروان",
-    "زانیار",
-    "ابوالفضل",
-    "ارسلان",
-    "بهزاد",
-    "سامان",
-    "شاهین",
-    "کیارش",
-    "حمیدرضا",
-    "کیکاوس",
-    "کوروش",
-    "کاوه",
-];
+
+let names = ["ریحانه","نیلوفر","نازنین","کیمیا","شهرزاد","فرناز","مهرناز","مژگان","معصومه","رومینا","گلچهره","دلاویز","سمانه","پریناز","بهنوش","سپیده","سعیده","سمیرا","گلشیفته","گلناز","بارانا","علیرضا","رادوین","ارشیا","امیرعلی","سامیار","عرفان","ماکان","سیروان","زانیار","ابوالفضل","ارسلان","بهزاد","سامان","شاهین","کیارش","حمیدرضا","کیکاوس","کوروش","کاوه"];
 
 let mistakeCounter = 0;
 let correctCounter = 0;
 
 let ArrayToFind = [];
-let selectedName = "";
 
-const endGame = (txt) => {
-    endGameScreen.querySelector(".endGame-message").textContent = txt;
+const randomNumber = Math.floor(Math.random() * names.length);
 
-    endGameScreen.style.display = "flex";
-    setTimeout(() => {
-        endGameScreen.style.opacity = "1";
-    }, 500);
-};
+const selectedName = names[randomNumber];
+console.log(selectedName);
+
+// little help ;)
+document.querySelectorAll(".wordBox")[0].title = selectedName;
+document.querySelectorAll(".wordBox")[1].title = selectedName;
+
+for (let i = 0; i < selectedName.length; i++) {
+    // ['م', 'ه', 'ر', 'ن', 'ا', 'ز']
+    ArrayToFind.push(selectedName.charAt(i));
+
+    // Create correct boxes
+    const nameDiv = document.createElement("div");
+    // nameDiv.textContent = selectedName.charAt(i);
+    nameDiv.textContent = "-";
+    // console.log(nameDiv);
+    wordToFind.appendChild(nameDiv);
+
+    // Create false boxes
+    const letterDiv = document.createElement("div");
+    letterDiv.textContent = "-";
+    failedLetters.appendChild(letterDiv);
+}
 
 const btnFunc = (key) => {
     console.log(key.textContent);
 
     if (ArrayToFind.includes(key.textContent)) {
-        console.log("yes");
+        // console.log("yes");
 
         for (let i = 0; i < ArrayToFind.length; i++) {
             if (key.textContent === ArrayToFind[i]) {
+                correctCounter++;
+
                 document.querySelectorAll(".wordToFind div")[i].textContent =
                     ArrayToFind[i];
             }
         }
-
-        correctCounter++;
     } else {
+        // console.log("no");
+
         document.querySelectorAll(".failedLetters div")[
             mistakeCounter++
         ].textContent = key.textContent;
@@ -85,7 +63,7 @@ const btnFunc = (key) => {
     key.disabled = true;
 
     // lose
-    if (mistakeCounter >= ArrayToFind.length) {
+    if (mistakeCounter === ArrayToFind.length) {
         endGame(
             `متاسفانه با وجود تلاش خوبت نتونستی کلمه درست رو حدس بزنی. کلمه درست "${selectedName}" بود.`
         );
@@ -98,31 +76,17 @@ const btnFunc = (key) => {
     }
 };
 
-const randomNumber = Math.floor(Math.random() * names.length);
+// EndGame screen
+const endGame = (txt) => {
+    endGameScreen.querySelector(".endGame-message").textContent = txt;
 
-// console.log(names[randomNumber],names[randomNumber].length);
+    endGameScreen.style.display = "flex";
+    setTimeout(() => {
+        endGameScreen.style.opacity = "1";
+    }, 500);
+};
 
-selectedName = names[randomNumber];
-console.log(selectedName);
-
-// little help ;)
-document.querySelectorAll(".wordBox")[0].title = selectedName;
-document.querySelectorAll(".wordBox")[1].title = selectedName;
-
-for (let i = 0; i < selectedName.length; i++) {
-    ArrayToFind.push(selectedName.charAt(i));
-
-    const nameDiv = document.createElement("div");
-    // nameDiv.textContent = selectedName.charAt(i);
-    nameDiv.textContent = "-";
-    // console.log(nameDiv);
-    wordToFind.appendChild(nameDiv);
-
-    const letterDiv = document.createElement("div");
-    letterDiv.textContent = "-";
-    failedLetters.appendChild(letterDiv);
-}
-
+// Reload btn
 document.querySelector(".restartBtn").addEventListener("click", () => {
     setTimeout(() => {
         location.reload();
